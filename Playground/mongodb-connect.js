184032e76037b4801209e9cd625ebb9737fa436e
EC2 Default User <ec2-user@ip-172-31-40-57.us-east-2.compute.internal>
2018-06-07 18:54:47 +0000

    Código para criação do primeiro dado no mongo DB.

--- Playground/mongodb-connect.js
+++ Playground/mongodb-connect.js
@@ -5,6 +5,21 @@ MongoClient.connect('mongodb://localhost:27017/Tests', (err,db) =>{
     return console.log("Não conseguiu connectar."+err);
   }
   console.log("Conectado ao Mongodb server.");
+  
+  var data = {
+    titulo : "Tarefa 1",
+    texto: "Necessário aprender note.js com mongo para criar API's rapidamente.",
+    finalizado: false
+  }
+  
+  db.collection('Todos').insertOne(data, (err,result) => {
+    if(err){
+      return console.log('Não foi possivel criar tarefa.', err);
+    }
+    
+    console.log(JSON.stringify(result.ops, undefined, 2));
+    
+  });
 
   db.close();
 });
